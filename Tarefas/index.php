f7b0494c8bbcc9a7c84a5de3b8156e96b6b0dd58
VITOR <vitorsilvio2008@gmail.com>
2026-02-02 17:07:09 -0300

    Add files via upload

---
+++ Tarefas/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css" >
+</head>
+    <body>
+        <style>
+            button
+            {
+                height: 50px auto;
+                width: 100px;
+                margin: 5px auto;
+                margin-left: 550px ;
+            }   
+        </style>
+        <h1>INDEX</h1>
+        <hr>
+        <br>
+        <button type="submit">Botão2</button>
+        <button type="submit">Botão1</button>
+                <div class="formulario">
+                <h2>FORMULARIO</h2>
+                <hr>
+                <label>Nome</label>
+                <label>Nome</label>
+                <label>Nome</label>
+                <div>
+            </form> 
+    </body>
+</body>
+</html>
